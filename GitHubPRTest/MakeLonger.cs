d850068d838e5baf02bbeb7a0dafd283388c81b8
Luke Hansen <54725543+luhansen-ms@users.noreply.github.com>
2024-04-04 17:32:15 -0700

    Update MakeLonger.cs to change merge base

--- GitHubPRTest/MakeLonger.cs
+++ GitHubPRTest/MakeLonger.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitHubPRTest
 {
-    internal class MakeLonger
+    internal class MakeLonger // change merge base
     {
         public MakeLonger()
         {
